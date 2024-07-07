59866d0b517cb0db741ab1820a818bf722439295
Adel <adel.garipovazn@mail.ru>
2024-07-07 23:47:06 +0300

    Add characters base mobx store

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,52 +1,32 @@
-import {useEffect, useState} from "react";
-
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
 import CharactersFilter from "../../components/CharactersFilter";
-import {ICharacter} from "../../types/character.ts";
-
-import {Grid, Box, CircularProgress} from "@mui/material";
-import {getAllCharacters} from "../../services";
+import { Grid, Box, CircularProgress } from "@mui/material";
 import CharactersList from "../../components/CharactersList";
+import charactersStore from "../../stores/CharactersStore";
 
-const MainPage = () => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [characters, setCharacters] = useState<ICharacter[]>([]);
+const MainPage = observer(() => {
+    const { isLoading, characters, fetchCharacters } = charactersStore;
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                setTimeout(async () => {
-                    const data = await getAllCharacters();
-                    setCharacters(data.results);
-                    setLoading(false);
-                }, 1000);
-            } catch (error) {
-                console.error("Error fetching characters:", error);
-            }
-        };
+        fetchCharacters();
+    }, [fetchCharacters]);
 
-        fetchData();
-    }, []);
-
-    return(
+    return (
         <Box sx={{ flexGrow: 1 }}>
+            <CharactersFilter />
 
-            <CharactersFilter/>
-
-            {loading
-                ? (
-                    <>
-                        <Grid container justifyContent="center">
-                            <Grid item>
-                                <CircularProgress />
-                            </Grid>
-                        </Grid>
-                    </>
-                )
-                : <CharactersList items={characters}/>
-            }
+            {isLoading ? (
+                <Grid container justifyContent="center">
+                    <Grid item>
+                        <CircularProgress />
+                    </Grid>
+                </Grid>
+            ) : (
+                <CharactersList items={characters} />
+            )}
         </Box>
     );
-}
+});
 
 export default MainPage;
---
+++ src/stores/CharactersStore/CharactersStore.ts
@@ -0,0 +1,40 @@
+import { action, computed, makeObservable, observable, runInAction } from "mobx";
+import { ICharacter } from "../../types/character";
+import { getAllCharacters } from "../../services";
+
+class CharactersStore {
+    isLoading: boolean = false;
+    characters: ICharacter[] = [];
+    totalCount: number = 0;
+
+    constructor() {
+        makeObservable(this, {
+            isLoading: observable,
+            characters: observable,
+            fetchCharacters: action.bound,
+            charactersTotalCount: computed,
+        });
+    }
+
+    get charactersTotalCount() {
+        return this.totalCount * 2;
+    }
+
+    async fetchCharacters(): Promise<void> {
+        this.isLoading = true;
+        try {
+            const data = await getAllCharacters();
+            runInAction(() => {
+                this.characters = data.results;
+                this.isLoading = false;
+            });
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.isLoading = false;
+            });
+        }
+    }
+}
+
+export default new CharactersStore();
