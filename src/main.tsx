ed9aff6f52ed54fc40d060b464dd0332e077c816
Adel <adel.garipovazn@mail.ru>
2024-07-07 16:47:25 +0300

    Add createTheme config

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './index.ts'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './index.ts';
+import './config/configureMobx.ts';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+        },
+        secondary: {
+            main: '#dc004e',
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
