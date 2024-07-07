252699f7536f13cab611a5e25cecca30789928d0
Adel <adel.garipovazn@mail.ru>
2024-07-07 19:00:04 +0300

    Create types

---
+++ src/types/server.ts
@@ -0,0 +1,9 @@
+export interface IServerResponse<T> {
+    info: {
+        count: number;
+        pages: number;
+        next: string | null;
+        prev: string | null;
+    };
+    results: T[];
+}
