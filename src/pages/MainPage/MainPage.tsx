81ccfd082849daf0b6aea45b5c79e4eae67894e6
Adel <adel.garipovazn@mail.ru>
2024-07-07 22:46:37 +0300

    Refactor Main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,30 +1,50 @@
-import {Grid, Box, Paper} from "@mui/material";
+import {useEffect, useState} from "react";
 
-import { styled } from '@mui/material/styles';
 import CharactersFilter from "../../components/CharactersFilter";
+import {ICharacter} from "../../types/character.ts";
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+import {Grid, Box, CircularProgress} from "@mui/material";
+import {getAllCharacters} from "../../services";
+import CharactersList from "../../components/CharactersList";
 
 const MainPage = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [characters, setCharacters] = useState<ICharacter[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                setTimeout(async () => {
+                    const data = await getAllCharacters();
+                    setCharacters(data.results);
+                    setLoading(false);
+                }, 1000);
+            } catch (error) {
+                console.error("Error fetching characters:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return(
         <Box sx={{ flexGrow: 1 }}>
 
             <CharactersFilter/>
 
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={6}>
-                    <Item>xs=4</Item>
-                </Grid>
-            </Grid>
+            {loading
+                ? (
+                    <>
+                        <Grid container justifyContent="center">
+                            <Grid item>
+                                <CircularProgress />
+                            </Grid>
+                        </Grid>
+                    </>
+                )
+                : <CharactersList items={characters}/>
+            }
         </Box>
     );
 }
